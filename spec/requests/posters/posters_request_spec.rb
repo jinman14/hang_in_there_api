39a014ca5931a4a0d9cb58dd98ebfabdd9910b3c
Jacob Inman <jrinman6295@gmail.com>
2025-02-20 16:09:53 -0800

    asc and desc spec written. should we be using names?

--- spec/requests/posters/posters_request_spec.rb
+++ spec/requests/posters/posters_request_spec.rb
@@ -132,5 +132,27 @@ RSpec.describe "Posters endpoints" do
     expect(posters[:meta][:count]).to eq(3)
   end
 
-  it ""
+  it "can sort by asc" do
+    Poster.create!(name: 'YOU GOT THIS', description: 'We are learning!', price: 100.50, year: 2025, vintage: true, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+    Poster.create!(name: 'WE ARE HEROES', description: 'Defeat the evil code we do not understand!', price: 75.50, year: 2024, vintage: false, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+    Poster.create!(name: 'OR MAYBE NOT, IDK', description: 'But still dont lose hope!', price: 50.50, year: 2023, vintage: true, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+
+    get "/api/v1/posters?sort=asc"
+    
+    posters = JSON.parse(response.body, symbolize_names: true)
+
+    expect(posters[:data].first[:attributes][:name]).to eq('YOU GOT THIS')
+  end
+
+  it "can sort by desc" do
+    Poster.create!(name: 'YOU GOT THIS', description: 'We are learning!', price: 100.50, year: 2025, vintage: true, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+    Poster.create!(name: 'WE ARE HEROES', description: 'Defeat the evil code we do not understand!', price: 75.50, year: 2024, vintage: false, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+    Poster.create!(name: 'OR MAYBE NOT, IDK', description: 'But still dont lose hope!', price: 50.50, year: 2023, vintage: true, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+
+    get "/api/v1/posters?sort=desc"
+    
+    posters = JSON.parse(response.body, symbolize_names: true)
+
+    expect(posters[:data].first[:attributes][:name]).to eq('OR MAYBE NOT, IDK')
+  end
 end
