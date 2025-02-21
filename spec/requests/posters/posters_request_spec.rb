58f5ad8f49b7dabd755a496846e2ee33a25fda2f
Jacob Inman <jrinman6295@gmail.com>
2025-02-21 11:41:54 -0800

    name parameter spec written

--- spec/requests/posters/posters_request_spec.rb
+++ spec/requests/posters/posters_request_spec.rb
@@ -182,7 +182,16 @@ RSpec.describe "Posters endpoints" do
   end
 
   it "can filter by name parameter" do
-    
+    Poster.create!(name: 'YOU GOT THIS', description: 'We are learning!', price: 100.50, year: 2025, vintage: true, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+    Poster.create!(name: 'WE ARE HEROES', description: 'Defeat the evil code we do not understand!', price: 75.50, year: 2024, vintage: false, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+    Poster.create!(name: 'OR MAYBE NOT, IDK', description: 'But still dont lose hope!', price: 50.50, year: 2023, vintage: true, img_url: 'https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d')
+
+    get "/api/v1/posters?name=ot"
+
+    posters = JSON.parse(response.body, symbolize_names: true)
+
+    expect(posters[:data].count).to eq(2)
+    expect(posters[:data].first[:attributes][:name]).to eq('YOU GOT THIS')
   end
 
   it "can filter by min price" do
