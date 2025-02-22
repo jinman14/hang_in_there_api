bd0ea1d3a86b49adac1c5d28a7aecacc85ae9b7d
Jacob Inman <jrinman6295@gmail.com>
2025-02-22 09:17:31 -0800

    refactored for developer empathy

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::PostersController < ApplicationController
 
     render json: PosterSerializer.format_posters(posters)
   end
-
+  
   def show
     poster = Poster.find(params[:id])
     render json: PosterSerializer.format_single(poster)
